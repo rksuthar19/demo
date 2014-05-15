f3718b456cac66b6539c46018c58d972ef367d23
suthar <rksuthar19@gmail.com>
2014-05-16 01:17:48 +0530

    adding project files

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: suthar
+ * Date: 16/5/14
+ * Time: 1:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("demo project by Rakesh Kumar Suthar");
+    }
+}
